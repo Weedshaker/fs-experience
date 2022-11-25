06fd8faceb09c0bf2aa632112a558d28625a33a5
Weedshaker <weedshaker@gmail.com>
2022-11-25 18:46:54 +0100

    loader added

--- src/components/pages/CoinCalcGram.js
+++ src/components/pages/CoinCalcGram.js
@@ -8,6 +8,7 @@ export default class TutorialOne extends Page {
 
     this.inputId = 'gram-to-fiat-chf'
     this.changeListener = event => {
+      this.root.querySelector('.loader').classList.add('show')
       const input = event.composedPath()[0]
       this.dispatchEvent(new CustomEvent('request-' + input.getAttribute('id'), {
         detail: {
@@ -22,7 +23,10 @@ export default class TutorialOne extends Page {
     }
     this.inputIdListener = event => {
       let resultNode
-      if ((resultNode = this.root.querySelector('#result-buy-' + event.detail.id))) resultNode.value = event.detail.value.toFixed(2)
+      if ((resultNode = this.root.querySelector('#result-buy-' + event.detail.id))) {
+        resultNode.value = event.detail.value.toFixed(2)
+        this.root.querySelector('.loader').classList.remove('show')
+      }
     }
   }
 
@@ -65,6 +69,74 @@ export default class TutorialOne extends Page {
         font-size: 6em;
         text-align: center;
       }
+      .result {
+        position: relative;
+      }
+      .loader.show {
+        display: block;
+      }
+      /* https://projects.lukehaas.me/css-loaders/ */
+      .loader,
+      .loader:before,
+      .loader:after {
+        background: var(--color);
+        -webkit-animation: load1 1s infinite ease-in-out;
+        animation: load1 1s infinite ease-in-out;
+        width: 1em;
+        height: 4em;
+      }
+      .loader {
+        display: none;
+        color: var(--color);
+        text-indent: -9999em;
+        margin: 88px auto;
+        position: absolute;
+        left: 50%;
+        font-size: 11px;
+        -webkit-transform: translateZ(0);
+        -ms-transform: translateZ(0);
+        transform: translateZ(0);
+        -webkit-animation-delay: -0.16s;
+        animation-delay: -0.16s;
+      }
+      .loader:before,
+      .loader:after {
+        position: absolute;
+        top: 0;
+        content: '';
+      }
+      .loader:before {
+        left: -1.5em;
+        -webkit-animation-delay: -0.32s;
+        animation-delay: -0.32s;
+      }
+      .loader:after {
+        left: 1.5em;
+      }
+      @-webkit-keyframes load1 {
+        0%,
+        80%,
+        100% {
+          box-shadow: 0 0;
+          height: 4em;
+        }
+        40% {
+          box-shadow: 0 -2em;
+          height: 5em;
+        }
+      }
+      @keyframes load1 {
+        0%,
+        80%,
+        100% {
+          box-shadow: 0 0;
+          height: 4em;
+        }
+        40% {
+          box-shadow: 0 -2em;
+          height: 5em;
+        }
+      }
     `
   }
 
@@ -79,7 +151,8 @@ export default class TutorialOne extends Page {
       </div>
       <div class=equal>=</div>
       <p class=info>Bei 83.5% Silbergehalt, siehe "* Unterstützte Silbermünzen"</p>
-      <div class="input">
+      <div class="input result">
+        <div class="loader">Loading...</div>
         <input disabled type="number" id="result-buy-${this.inputId}" name="result-buy-${this.inputId}" placeholder="..."><span>&nbsp;CHF</span>
       </div>
       <p class=info>Keine Garantie bezüglich Richtigkeit der Umrechnung.</p>
